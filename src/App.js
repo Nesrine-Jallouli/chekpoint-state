cce21f411fb83b1d88c293a07fd2e28ed5f9d359
cat <nesrine.jallouli91@gmail.com>
2021-01-16 23:50:58 +0100

    chekpoint state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import picture from './NesrineProfil.jpg' ; 
+import chrono from './chrono.png' ;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  Persone = {
+    fullName: "Nesrine Jallouli" ,
+    bio : "Cité Hedi Nouira Borj Louzir Ariana",
+    profession : "Student in GO MY CODE",
+    imgSrc : picture
+  };
+ 
+    constructor( props ){
+        super( props )
+        this.state = { show : false , seconds: 0};
+        this.toggleDiv = this.toggleDiv.bind(this)
+    }
+
+    toggleDiv = () => {
+        const { show } = this.state;
+        this.setState( { show : !show} )
+        if (!show)
+        {
+          this.interval = setInterval(() => {
+            this.setState(prevState => ({
+              seconds: prevState.seconds + 1
+            }));
+          }, 1000);
+        }
+        else 
+        {
+          clearInterval(this.interval);
+        }
+        
+    }
+ 
+
+    render() {
+      if (this.state.show ) 
+        {
+          return (
+  
+              <div className="container">
+                  <br />
+                  <div className="chrono"> 
+                    <img src={chrono} alt="chronometre" width="40px"/> 
+                    <button>{this.state.seconds}</button>
+                  </div>
+                  <button onClick={ this.toggleDiv } class="btn btn-success">Cacher Profil</button>
+                  
+                  { this.state.show && <h1>Hello {this.Persone.fullName}</h1> }
+                  { this.state.show && <p>I'm from in {this.Persone.bio}</p>  }
+                  { this.state.show && <p>I'm {this.Persone.profession}</p>  }
+                  { this.state.show && <img src={this.Persone.imgSrc} alt="photo de profil" width="320px" />  }
+
+              </div>
+  
+          );
+        }  
+      else 
+        {
+          return (
+            <div className="container">
+               <button onClick={this.toggleDiv } class="btn btn-success"> Afficher Profil</button>
+            </div>
+            )
+        }
+  
+  }
 }
 
 export default App;
+
+
